8090f6045e5562cb3091264b1617287b83dc755e
Sanapala Mounika <138890767+Mouni560@users.noreply.github.com>
2025-01-25 20:22:55 +0530

    Time: 0 ms (100.00%) | Memory: 62.8 MB (63.40%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int maxArea = 0;
+        
+        while (left < right) {
+            // Calculate the area between the lines at left and right
+            int area = min(height[left], height[right]) * (right - left);
+            maxArea = max(maxArea, area);
+            
+            // Move the pointer pointing to the shorter line
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return maxArea;
+    }
+};
